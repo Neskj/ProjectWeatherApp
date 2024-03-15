d9134b72db63d650a0b3caa5a8d374e2e86cf6ee
Neskj <pronin.qq@gmail.com>
2024-03-15 22:30:03 +0300

    add base config

---
+++ ProjectWeatherApp/src/test/java/neskj/ProjectWeatherApp/ProjectWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.ProjectWeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
