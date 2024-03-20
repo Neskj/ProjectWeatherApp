e64ca8e55a3cbc5003938a24dbcbaeaea09bbaa6
Neskj <pronin.qq@gmail.com>
2024-03-20 13:03:54 +0300

    add pojo

--- ProjectWeatherApp/src/main/java/neskj/ProjectWeatherApp/Service/GSONconverter.java
+++ ProjectWeatherApp/src/main/java/neskj/ProjectWeatherApp/Service/GSONconverter.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GSONconverter implements Converter {
-
+    
     @Override
     public Object convert(String json) {
+
         System.out.println("JSON is come to ---->>> converter "+json);
+
+
         return json;
     }
 }
