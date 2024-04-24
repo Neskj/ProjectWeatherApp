c39862fd6460cd9f44de1a6c86665eeafe0fe440
Neskj <pronin.qq@gmail.com>
2024-04-24 09:05:49 +0300

    add test to WeatherController class

---
+++ ProjectWeatherApp/src/test/java/neskj/ProjectWeatherApp/Controllers/WeatherControllerTest.java
@@ -0,0 +1,33 @@
+package neskj.ProjectWeatherApp.Controllers;
+
+import neskj.ProjectWeatherApp.Interfaces.Converter;
+import neskj.ProjectWeatherApp.Proxy.MessageProxy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+public class WeatherControllerTest {
+
+    @Mock
+    private Converter converter;
+    @Mock
+    private MessageProxy messageProxy;
+    @InjectMocks
+    private WeatherController weatherController;
+
+    @Test
+    public void getWeatherReturnPage(){
+
+        assertNotNull(weatherController.getWhether());
+        assertEquals("weather.html",weatherController.getWhether());
+    }
+
+
+}
