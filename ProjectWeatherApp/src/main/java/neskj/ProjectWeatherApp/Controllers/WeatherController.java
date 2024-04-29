b929be6ce295e608d5adca038903bc94a37a0bae
Neskj <pronin.qq@gmail.com>
2024-04-29 21:06:16 +0300

    opimization controller's constructor

--- ProjectWeatherApp/src/main/java/neskj/ProjectWeatherApp/Controllers/WeatherController.java
+++ ProjectWeatherApp/src/main/java/neskj/ProjectWeatherApp/Controllers/WeatherController.java
@@ -13,18 +13,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WeatherController {
 
-    private MessageProxy proxyResourse;
+    private final MessageProxy proxyResourse;
     private final Converter converter;
     @Value("${app.key}")
     private String key;
 
     @Autowired
-    WeatherController(MessageProxy proxyResourse,Converter converter){
-        this.proxyResourse=proxyResourse;
+    WeatherController(Converter converter){
+        this.proxyResourse=Feign.builder().encoder(new SpringFormEncoder())
+                .target(MessageProxy.class, "https://api.openweathermap.org");
         this.converter=converter;
     }
 
@@ -37,8 +39,6 @@ public class WeatherController {
     @PostMapping("/weather")
     public String postWhether(@RequestParam String city, Model page) {
 
-        proxyResourse = Feign.builder().encoder(new SpringFormEncoder())
-                .target(MessageProxy.class, "https://api.openweathermap.org");
         String json = proxyResourse.proxySend(city, key);
         Request request = converter.convert(json);
         String temp = Answer.TEMP.getResponse() + request.getTemp();
@@ -52,4 +52,12 @@ public class WeatherController {
         page.addAttribute("tempMax", tempMax);
         return "weather.html";
     }
+   /*
+    @PostMapping("/whatweather")
+    @ResponseBody
+    public Request whatWeather(@RequestParam(required = false) String city){
+
+    }
+
+    */
 }
